de2790796a545e00be8c5116f32ce21b9fbdc63f
Ira <kalmykova_ira@ukr.net>
2019-08-13 18:44:57 +0300

    Updates

---
+++ precache-manifest.f8fd06c7ad0fc47edc0eb577fd10191e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244bae568eaceaec2b3e15b9bb549dd9",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "d5f41c93801b9abc8e78",
+    "url": "/react_phone-catalog/static/css/main.f94ec5bc.chunk.css"
+  },
+  {
+    "revision": "d7b654e110a1369bd00a",
+    "url": "/react_phone-catalog/static/js/2.ca9ac7b8.chunk.js"
+  },
+  {
+    "revision": "d5f41c93801b9abc8e78",
+    "url": "/react_phone-catalog/static/js/main.735df099.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
